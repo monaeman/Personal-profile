c614293dbed5b6a973e44385f52fc142ec456cc2
monaeman <mona.eman2018@gmail.com>
2023-08-15 05:24:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Home />
+        <About />
+      </div>
+    </>
+  );
+}
+
+export default App;
